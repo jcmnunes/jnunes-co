20aaf95b71fecb5f763b062ca5809b0a093cc748
José Nunes <jnunes@dodoc.com>
2017-12-01 00:45:46 +0000

    Change to variables.env

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
+// import environmental variables from our variables.env file
+require('dotenv').config({ path: 'variables.env' });
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(process.env.DATABASE);
 
 const app = express();
 
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey],
+    keys: [process.env.COOKIE_KEY],
   }),
 );
 app.use(passport.initialize());
@@ -21,5 +23,5 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
-const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
@@ -17,9 +16,9 @@ passport.deserializeUser(async (id, done) => {
 passport.use(
   new GoogleStrategy(
     {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: 'https://jnunes.xyz/auth/google/callback',
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ googleID: profile.id });
